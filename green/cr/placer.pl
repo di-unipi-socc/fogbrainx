6558e28ddcb64f7fdcf68bb237b83623e940ee9a
jacopo-massa <jacopomassa97@gmail.com>
2023-04-20 14:52:48 +0000

    add node ranking

--- green/cr/placer.pl
+++ green/cr/placer.pl
@@ -6,6 +6,14 @@ placement([S|Ss],P,(AllocHW,AllocBW),R, Rates, Placement) :-
     placement(Ss,[on((S,V),N)|P],(AllocHW,AllocBW),TmpR, Rates, Placement).
 placement([],P,_,R,R,RP) :- reverse(P, RP). 
 
+rankNodes(RankedNodes):- findall(N, node(N,_,_,_), Ns), nodesFootprint(Ns, NFs), sort(NFs, RankedNodes).
+
+nodesFootprint([N|Ns], [(F,N)|NFs]) :- 
+    energySourceMix(N,Sources),
+    findall(E, (member((P,S),Sources), emissions(S, MU), E = P*MU), Es), sum_list(Es, F),
+    nodesFootprint(Ns,NFs).
+nodesFootprint([],[]).
+
 nodeOk(S,N,V,P,AllocHW) :-
     service(S,V,SWReqs,HWReqs,IoTReqs),
     node(N,SWCaps,HWCaps,IoTCaps),
